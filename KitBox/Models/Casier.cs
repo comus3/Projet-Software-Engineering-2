718e30f608650216f9519cba25b964fdab50e665
petibato <20278@ecam.be>
2024-02-09 15:36:57 +0100

    Implémentation de base de MAUI, création d'une page Catalogue Client

---
+++ KitBox/Models/Casier.cs
@@ -0,0 +1,27 @@
+﻿namespace KitBox.Models;
+public class Casier
+{
+    // Les attributs de la classe Casier dépendront du diagramme de classe et de la base de données
+    public string Code { get; set; }
+    public string Hauteur { get; set; }
+    public string Profondeur { get; set; }
+    public string Largeur { get; set; }
+    public string Couleur { get; set; }
+    public string Prix { get; set; }
+    public string Stock { get; set; }
+    public string Quantite { get; set; }
+    public string Image { get; set;}
+
+    public Casier(string code, string hauteur, string profondeur, string largeur, string couleur, string prix, string stock, string quantite, string image)
+    {
+        Code = code;
+        Hauteur = hauteur;
+        Profondeur = profondeur;
+        Largeur = largeur;
+        Couleur = couleur;
+        Prix = prix;
+        Stock = stock;
+        Quantite = quantite;
+        Image = image;
+    }
+}
---
+++ KitBox/Views/Customer_catalog.xaml.cs
@@ -0,0 +1,12 @@
+namespace KitBox.Views;
+
+public partial class Customer_catalog : ContentPage
+{
+
+	public Customer_catalog()
+	{
+		Title = "Catalogue";
+		InitializeComponent();
+		BindingContext = this;
+	}
+}
