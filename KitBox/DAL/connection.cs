f2decb7a53bda4683b3feffdcca009dd3a1883c9
comus3 <come.plantin.carrenard@gmail.com>
2024-02-12 13:13:31 +0100

    connection first test

--- KitBox/DAL/connection.cs
+++ KitBox/DAL/connection.cs
@@ -1,4 +1,52 @@
 using MySql.Data.MySqlClient;
 using System;
 using System.Data;
+namespace DAL;
 
+public static class Connection
+{
+    private static readonly string Server="localhost;Port=3306;Database=kitboxdb;Uid=root;Pwd=password;Charset=utf8;SslMode=Preferred;";
+
+    private static readonly string connectionString = Server;
+
+    public static MySqlConnection GetConnection()
+    {
+        MySqlConnection connection = new MySqlConnection(connectionString);
+        return connection;
+    }
+
+    public static void TestConnection()
+    {
+        using (MySqlConnection connection = GetConnection())
+        {
+            try
+            {
+                connection.Open();
+                Console.WriteLine("Connection successful!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error connecting to database: {ex.Message}");
+            }
+        }
+    }
+    public static DataTable ExecuteQuery(string query)
+    {
+        DataTable dataTable = new DataTable();
+        using (MySqlConnection connection = GetConnection())
+        {
+            try
+            {
+                connection.Open();
+                MySqlCommand command = new MySqlCommand(query, connection);
+                MySqlDataAdapter adapter = new MySqlDataAdapter(command);
+                adapter.Fill(dataTable);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error executing query: {ex.Message}");
+            }
+        }
+        return dataTable;
+    }
+}
