7e5e9c6ad6f7c25242320b905786165fe8115611
Soraya-j <21255@ecam.be>
2024-02-15 14:09:06 +0100

    Function add new casier

--- KitBox/Views/Form.xaml.cs
+++ KitBox/Views/Form.xaml.cs
@@ -2,6 +2,8 @@ namespace KitBox.Views;
 
 public partial class Form : ContentPage
 {
+	int count = 1;
+	private int entryCount = 1;
 
 	public Form()
 	{
@@ -29,8 +31,16 @@ public partial class Form : ContentPage
 
 	private void OnCreateNewCasierClicked(object sender, EventArgs e)
 	{
-		
+		count++;
+		Label newLabel = new Label();
+		newLabel.Text = "Casier number " + count.ToString();
+
+		Entry entry = new Entry();
+        entry.Placeholder = "Height " + count;
+
+		labelContainer.Children.Add(newLabel);
+		labelContainer.Children.Add(entry);
 	}
 	
-
+	 
 }
