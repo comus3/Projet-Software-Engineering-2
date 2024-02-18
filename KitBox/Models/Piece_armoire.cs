028ecc89807323b70144aba4e82406870dc58e4e
zekhayoub <22371@ecam.be>
2024-02-18 15:20:09 +0100

    modif

--- KitBox/Models/Piece_armoire.cs
+++ KitBox/Models/Piece_armoire.cs
@@ -2,12 +2,12 @@
 
 public class Piece_armoire
 {
-    public string Type { get; set; }
-    public double Longueur { get; set; }
-    public double Largeur { get; set; }
-    public double Prix { get; set; }
+    private string Type { get; set; }
+    private double Longueur { get; set; }
+    private double Largeur { get; set; }
+    private double Prix { get; set; }
 
-    
+
     public Piece_armoire(string type, double longueur, double largeur, double prix)
     {
         Type = type;
