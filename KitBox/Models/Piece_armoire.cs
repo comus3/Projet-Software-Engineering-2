08fac08fd88d685877da6d70d0e4a9254dd66267
zekhayoub <22371@ecam.be>
2024-02-18 15:16:20 +0100

    rajour de la classe piece_casier et piece_armoire

--- KitBox/Models/Piece_armoire.cs
+++ KitBox/Models/Piece_armoire.cs
@@ -1,20 +1,19 @@
-﻿using System;
+﻿namespace KitBox;
 
-namespace KitBox
+public class Piece_armoire
 {
-    public class Piece_armoire
-    {
-        public string Type { get; set; }
-        public double Longueur { get; set; }
-        public double Largeur { get; set; }
-        public double Prix { get; set; }
+    public string Type { get; set; }
+    public double Longueur { get; set; }
+    public double Largeur { get; set; }
+    public double Prix { get; set; }
 
-        public Piece_armoire(string type, double longueur, double largeur, double prix)
-        {
-            Type = type;
-            Longueur = longueur;
-            Largeur = largeur;
-            Prix = prix;
-        }
+    
+    public Piece_armoire(string type, double longueur, double largeur, double prix)
+    {
+        Type = type;
+        Longueur = longueur;
+        Largeur = largeur;
+        Prix = prix;
     }
 }
+
--- KitBox/Models/Piece_casier.cs
+++ KitBox/Models/Piece_casier.cs
@@ -1,6 +1,19 @@
 ﻿namespace KitBox;
 
-public class Piece_casier
-{
 
+
+public class Piece_casier
+    {
+    public string Type { get; set; }
+    public double Longueur { get; set; }
+    public double Largeur { get; set; }
+    public double Prix { get; set; }
+
+    public Piece_casier(string type, double longueur, double largeur, double prix)
+    {
+        Type = type;
+        Longueur = longueur;
+        Largeur = largeur;
+        Prix = prix;
+    }
 }
