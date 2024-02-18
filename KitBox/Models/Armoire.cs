e1b220d32fc60f8ef103f3b59e4fb6daf2841109
zekhayoub <22371@ecam.be>
2024-02-18 15:03:33 +0100

    classe armoire

--- KitBox/Models/Armoire.cs
+++ KitBox/Models/Armoire.cs
@@ -1,6 +1,22 @@
-﻿namespace KitBox;
+﻿using KitBox.Models;
 
-public class Armoire
+namespace KitBox
 {
+    public class Armoire
+    {
+        private List<Casier> Casiers { get; set; }
+        private int Longueur { get; set; }
+        private int Largeur { get; set; }
+        private int Prix { get; set; }
+        private List<Armoire> PiecesArmoire { get; set; }
 
+        public Armoire(List<Casier> casiers, int longueur, int largeur, int prix, List<Armoire> piecesArmoire)
+        {
+            Casiers = casiers;
+            Longueur = longueur;
+            Largeur = largeur;
+            Prix = prix;
+            PiecesArmoire = piecesArmoire;
+        }
+    }
 }
