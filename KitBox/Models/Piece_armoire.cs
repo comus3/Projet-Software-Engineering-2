2ae5c41457aa8231b4cd14a0a564f354da07e025
zekhayoub <22371@ecam.be>
2024-02-18 15:08:58 +0100

    Ajout de la classe Piece_armoire pour représenter les pièces d'armoire

--- KitBox/Models/Piece_armoire.cs
+++ KitBox/Models/Piece_armoire.cs
@@ -1,6 +1,20 @@
-﻿namespace KitBox;
+﻿using System;
 
-public class Piece_armoire
+namespace KitBox
 {
+    public class Piece_armoire
+    {
+        public string Type { get; set; }
+        public double Longueur { get; set; }
+        public double Largeur { get; set; }
+        public double Prix { get; set; }
 
+        public Piece_armoire(string type, double longueur, double largeur, double prix)
+        {
+            Type = type;
+            Longueur = longueur;
+            Largeur = largeur;
+            Prix = prix;
+        }
+    }
 }
