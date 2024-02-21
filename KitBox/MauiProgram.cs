49673d7f30d264f3e9dba482b4dcad5d96274b36
Côme Plantin-Carrenard <120224883+comus3@users.noreply.github.com>
2024-02-21 21:12:27 +0100

    removed the temporary tests for merging

--- KitBox/MauiProgram.cs
+++ KitBox/MauiProgram.cs
@@ -11,12 +11,6 @@ public static class MauiProgram
 	{
 		//FOR DEBUGGING DELETE BEFORE MERGE
 		Connection.TestConnection();
-		Connection con = new Connection();
-		Armoire arm = new Armoire(con);
-		Displayer.DisplayData(arm.Load(1));
-		Dictionary<string,object> dico = new Dictionary<string,object>();
-		dico["largeur"] = 2;
-		Displayer.DisplayData(arm.LoadAll(dico));
 		var builder = MauiApp.CreateBuilder();
 		builder
 			.UseMauiApp<App>()
