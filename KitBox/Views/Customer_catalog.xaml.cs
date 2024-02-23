2f0d068df2de3b1a07aca8bafd57f0eb084e4557
zekhayoub <22371@ecam.be>
2024-02-23 17:59:53 +0100

    rajout d'un mesage d'erreur

--- KitBox/Views/Customer_catalog.xaml.cs
+++ KitBox/Views/Customer_catalog.xaml.cs
@@ -1,20 +1,33 @@
-namespace KitBox.Views;
+using Microsoft.Maui.Controls;
+using System;
 
-public partial class Customer_catalog : ContentPage
+namespace KitBox.Views
 {
+    public partial class Customer_catalog : ContentPage
+    {
+        public Customer_catalog()
+        {
+            InitializeComponent();
+            BindingContext = this;
+        }
 
-	public Customer_catalog()
-	{
-		InitializeComponent();
-		BindingContext = this;
-	}
+        private void OnCreateCasierClicked(object sender, EventArgs e)
+        {
+            double length;
+            double width;
 
-	private void OnCreateCasierClicked(object sender, EventArgs e)
-	{
-		double length = Convert.ToDouble(lengthEntry.Text);
-		double width = Convert.ToDouble(widthEntry.Text);
-
-		DisplayAlert("Your Cabinet is", $"Length: {length}, Width: {width}", "OK");
-		Navigation.PushAsync(new Form());
-	}
+            // Vérifier si les entrées sont valides
+            if (double.TryParse(lengthEntry.Text, out length) && double.TryParse(widthEntry.Text, out width))
+            {
+                // Les entrées sont valides, afficher une alerte avec les dimensions et naviguer vers la page Form
+                DisplayAlert("Your Cabinet is", $"Length: {length}, Width: {width}", "OK");
+                Navigation.PushAsync(new Form());
+            }
+            else
+            {
+                // Les entrées ne sont pas valides, afficher un message d'erreur
+                DisplayAlert("Error", "Please enter valid dimensions.", "OK");
+            }
+        }
+    }
 }
