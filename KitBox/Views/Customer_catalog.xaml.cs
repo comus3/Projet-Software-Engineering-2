c4fa069fcb7d2226012d61d1430ec2684ee2c04a
zekhayoub <22371@ecam.be>
2024-02-24 16:37:10 +0100

    Lier base de donnée

--- KitBox/Views/Customer_catalog.xaml.cs
+++ KitBox/Views/Customer_catalog.xaml.cs
@@ -1,14 +1,21 @@
 using Microsoft.Maui.Controls;
+using DAL; // Importez le namespace où se trouve votre modèle
 using System;
+using System.Collections.Generic;
 
 namespace KitBox.Views
 {
     public partial class Customer_catalog : ContentPage
     {
+        private Casier casier;
+
         public Customer_catalog()
         {
             InitializeComponent();
             BindingContext = this;
+
+            Connection connection = new Connection();
+            casier = new Casier(connection);
         }
 
         private void OnCreateCasierClicked(object sender, EventArgs e)
@@ -19,8 +26,21 @@ namespace KitBox.Views
             // Vérifier si les entrées sont valides
             if (double.TryParse(lengthEntry.Text, out length) && double.TryParse(widthEntry.Text, out width))
             {
-                // Les entrées sont valides, afficher une alerte avec les dimensions et naviguer vers la page Form
-                DisplayAlert("Your Cabinet is", $"Length: {length}, Width: {width}", "OK");
+                // Les entrés sont valide, préparez les données à insérer dans la base de données
+                Dictionary<string, object> values = new Dictionary<string, object>();
+                values["length"] = length;
+                values["width"] = width;
+
+                //  méthode Update pour mettre à jour les attributs de l'objet casier avec les nouvelles valeurs
+                casier.Update(values);
+
+                //  méthode Insert pour insérer le nouveau casier dans la base de données
+                casier.Insert();
+
+                // Affichez une alerte avec les dimensions et informez l'utilisateur que le casier a été créé
+                DisplayAlert("Success", $"Your Cabinet is\nLength: {length}, Width: {width}\n\nThe cabinet has been created successfully!", "OK");
+
+                // Naviguez vers la page Form pour continuer la configuration du casier
                 Navigation.PushAsync(new Form());
             }
             else
--- KitBox/Views/Form.xaml.cs
+++ KitBox/Views/Form.xaml.cs
@@ -1,93 +1,97 @@
 using Microsoft.UI.Xaml;
+using DAL;
 
-namespace KitBox.Views;
-
-public partial class Form : ContentPage
+namespace KitBox.Views
 {
-	int count = 1;
-	string[] options = { "Option 1", "Option 2", "Option 3" };
-	List<string> doorOptions = new List<string>
-		{
-			"Bleu Turquoise",
-			"Mauve",
-			"Rouge sang"
-		};
-	public Form()
-	{
-		InitializeComponent();
-		//BindingContext = this;
-	}
+    public partial class Form : ContentPage
+    {
+        int count = 1;
+        string[] options = { "Option 1", "Option 2", "Option 3" };
+        List<string> doorOptions = new List<string>
+        {
+            "Bleu Turquoise",
+            "Mauve",
+            "Rouge sang"
+        };
 
-	private void OptionsPicker_SelectedIndexChanged(object sender, EventArgs e)
+        Connection connection; // Déclaration de la connexion à la base de données
+
+        public Form()
+        {
+            InitializeComponent();
+            connection = new Connection(); // Initialisation de la connexion
+            //BindingContext = this;
+        }
+
+        private void OptionsPicker_SelectedIndexChanged(object sender, EventArgs e)
         {
             var selectedOption = (string)OptionsPicker.SelectedItem;
             //ResultLabel.Text = $"Vous avez choisi l'option : {selectedOption}";
         }
-	
-	private void OptionsDoorPicker_SelectedIndexChanged(object sender, EventArgs e)
+
+        private void OptionsDoorPicker_SelectedIndexChanged(object sender, EventArgs e)
         {
             var selectedOption = (string)OptionsDoorPicker.SelectedItem;
             //ResultDoorLabel.Text = $"Vous avez choisi l'option : {selectedOption}";
         }
 
-	private void OnFinishClicked(object sender, EventArgs e)
-	{
+        private void OnFinishClicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new FinishPage());
+        }
 
-		Navigation.PushAsync(new FinishPage());
-	}
+        private void OnCreateNewCasierClicked(object sender, EventArgs e)
+        {
+            count++;
+            Label newLabel = new Label();
+            newLabel.Text = "Locker number " + count.ToString();
+            newLabel.FontAttributes = FontAttributes.Bold;
+            newLabel.HorizontalOptions = LayoutOptions.Center;
 
-	private void OnCreateNewCasierClicked(object sender, EventArgs e)
-	{
-		count++;
-		Label newLabel = new Label();
-		newLabel.Text = "Locker number " + count.ToString();
-		newLabel.FontAttributes = FontAttributes.Bold;
-		newLabel.HorizontalOptions = LayoutOptions.Center;
+            Entry entry = new Entry();
+            entry.Placeholder = "Height " + count;
 
-		Entry entry = new Entry();
-        entry.Placeholder = "Height " + count;
+            Label Color = new Label();
+            Color.Text = "Choose a color : ";
 
-		Label Color = new Label();
-		Color.Text = "Choose a color : ";	
+            Picker colorPicker = new Picker();
+            foreach (string option in options)
+            {
+                colorPicker.Items.Add(option);
+            }
 
-		Picker colorPicker = new Picker();
-		foreach (string option in options)
-		{
-			colorPicker.Items.Add(option);
-		}
+            Label Door = new Label();
+            Door.Text = "Include a door";
+            Door.VerticalOptions = LayoutOptions.Center;
+            CheckBox checkBox = new CheckBox();
 
-		Label Door = new Label();
-		Door.Text = "Include a door";
-		Door.VerticalOptions = LayoutOptions.Center;
-		CheckBox checkBox = new CheckBox();
-		
-		HorizontalStackLayout stackPanel = new HorizontalStackLayout();
-		stackPanel.Children.Add(Door);
-		stackPanel.Children.Add(checkBox);
+            HorizontalStackLayout stackPanel = new HorizontalStackLayout();
+            stackPanel.Children.Add(Door);
+            stackPanel.Children.Add(checkBox);
 
+            Label colorDoor = new Label();
+            colorDoor.Text = "Choose a color for the door number " + count.ToString() + " : ";
+            colorDoor.VerticalOptions = LayoutOptions.Center;
+            Picker colorDoorPicker = new Picker();
+            foreach (string option in doorOptions)
+            {
+                colorDoorPicker.Items.Add(option);
+            }
 
-		Label colorDoor = new Label();
-		colorDoor.Text = "Choose a color for the door number " + count.ToString() + " : ";
-		colorDoor.VerticalOptions=LayoutOptions.Center;
-		Picker colorDoorPicker = new Picker();
-		foreach (string option in doorOptions)
-		{
-			colorDoorPicker.Items.Add(option);
-		}
-
-		 checkBox.CheckedChanged += (sender, e)=> {
-		 	bool isChecked = ((CheckBox)sender).IsChecked;
-		 	if (isChecked)
-		 	{
-		 		stackPanel.Children.Add(colorDoor);
-		 		stackPanel.Children.Add(colorDoorPicker);
-			}
-			else
-			{
-				stackPanel.Children.Remove(colorDoor);
-				stackPanel.Children.Remove(colorDoorPicker);
-			}
-		};
+            checkBox.CheckedChanged += (sender, e) =>
+            {
+                bool isChecked = ((CheckBox)sender).IsChecked;
+                if (isChecked)
+                {
+                    stackPanel.Children.Add(colorDoor);
+                    stackPanel.Children.Add(colorDoorPicker);
+                }
+                else
+                {
+                    stackPanel.Children.Remove(colorDoor);
+                    stackPanel.Children.Remove(colorDoorPicker);
+                }
+            };
 
 		// checkBox.CheckedChanged += (sender, e) => {
 		// 	bool isChecked = ((CheckBox)sender).IsChecked;
@@ -110,17 +114,23 @@ public partial class Form : ContentPage
 		// 		}
 		// 	}
 		// };
-		
-		labelContainer.Children.Add(newLabel);
-		labelContainer.Children.Add(entry);
-		labelContainer.Children.Add(Color);
-		labelContainer.Children.Add(colorPicker);
-		labelContainer.Children.Add(stackPanel);
-	}
-	private void OnDoorCheckboxCheckedChanged(object sender, CheckedChangedEventArgs e)
-	{
-		bool isChecked = e.Value; // 'Value' is a property of CheckedChangedEventArgs indicating the new checked state.
-		doorColorLabel.IsVisible = isChecked;
-		OptionsDoorPicker.IsVisible = isChecked;
-	}
+
+            labelContainer.Children.Add(newLabel);
+            labelContainer.Children.Add(entry);
+            labelContainer.Children.Add(Color);
+            labelContainer.Children.Add(colorPicker);
+            labelContainer.Children.Add(stackPanel);
+
+            // Création d'un nouvel objet Casier et insertion dans la base de données
+            Casier casier = new Casier(connection);
+            casier.Insert();
+        }
+
+        private void OnDoorCheckboxCheckedChanged(object sender, CheckedChangedEventArgs e)
+        {
+            bool isChecked = e.Value; // 'Value' is a property of CheckedChangedEventArgs indicating the new checked state.
+            doorColorLabel.IsVisible = isChecked;
+            OptionsDoorPicker.IsVisible = isChecked;
+        }
+    }
 }
