52c7fee3e54d50a5e8b429326e59b70aa548e309
GaripA <garip.akkaya9@gmail.com>
2024-02-28 21:00:51 +0100

    commit garip catalog

--- KitBox/Views/Customer_catalog.xaml.cs
+++ KitBox/Views/Customer_catalog.xaml.cs
@@ -1,6 +1,5 @@
 using Microsoft.Maui.Controls;
-
-using DAL; // Importez le namespace où se trouve votre modèle
+using DAL;
 using System;
 using System.Collections.Generic;
 using System.Data;
@@ -12,12 +11,9 @@ namespace KitBox.Views
     {
         private Casier casier;
         private Connection con;
-
         private Commande com;
         private object comPk;
-
-        private List<Object> listArmoirePk ;
-
+        private List<Object> listArmoirePk;
 
         public Customer_catalog()
         {
@@ -30,54 +26,61 @@ namespace KitBox.Views
             con = new Connection();
 
             // Initialisez une commande
-            Commande com = new Commande(con);
+            com = new Commande(con);
             Dictionary<string, object> infoClient = new Dictionary<string, object>();
             infoClient["date"] = 240224;
             com.Update(infoClient);
             DataTable data = com.Insert();
             Displayer.DisplayData(data);
-            this.com = com;
-            this.comPk = data.Rows[0].ItemArray[0];
-            this.listArmoirePk = new List<Object>();
-            //Displayer.DisplayData(com.getLastPk());
+            comPk = data.Rows[0].ItemArray[0];
+            listArmoirePk = new List<Object>();
         }
 
         private void OnCreateCasierClicked(object sender, EventArgs e)
         {
-            double length;
-            double width;
-
-            // Vérifiez si les entrées sont valides
-            if (double.TryParse(lengthEntry.Text, out length) && double.TryParse(widthEntry.Text, out width))
+            // Vérifiez si les dimensions sont sélectionnées
+            if (lengthPicker.SelectedItem == null || widthPicker.SelectedItem == null)
             {
-                // Les entrées sont valides, préparez les données à insérer dans la base de données
+                // Affichez un message d'erreur si une ou les deux dimensions ne sont pas choisies
+                DisplayAlert("Error", "Make sure to choose both dimensions.", "OK");
+                return;
+            }
+
+            double width, length;
+            // Convertissez les valeurs sélectionnées en chaînes de caractères, puis analysez-les en tant que double
+            if (!double.TryParse(lengthPicker.SelectedItem.ToString(), out length) || !double.TryParse(widthPicker.SelectedItem.ToString(), out width))
+            {
+                // Affichez un message d'erreur si les valeurs sélectionnées ne sont pas valides
+                DisplayAlert("Error", "Please enter valid dimensions.", "OK");
+                return;
+            }
+
+            // Vérifiez que la largeur est inférieure ou égale à la longueur
+            if (width <= length)
+            {
+                // Les dimensions sont valides, continuez le traitement
+                Armoire armoire = new Armoire(con);
                 Dictionary<string, object> values = new Dictionary<string, object>();
                 values["longueur"] = length;
                 values["largeur"] = width;
                 values["commande"] = comPk;
-
-                // Initialisez un nouveau casier
-                Armoire armoire = new Armoire(con);
-
-                // Mettez à jour les attributs de l'objet casier avec les nouvelles valeurs
                 armoire.Update(values);
 
-                // Insérez le nouveau casier dans la base de données
-                this.listArmoirePk.Add(armoire.Insert().Rows[0].ItemArray[0]);
-                Logger.WriteToFile(this.listArmoirePk);
+                listArmoirePk.Add(armoire.Insert().Rows[0].ItemArray[0]);
+                Logger.WriteToFile(listArmoirePk);
 
                 // Affichez une alerte avec les dimensions et informez l'utilisateur que le casier a été créé
                 DisplayAlert("Success", $"Your Cabinet is\nLength: {length}, Width: {width}\n\nThe cabinet has been created successfully!", "OK");
 
-                // Naviguez vers la page Form pour continuer la configuration du casier
-                object lastArmoireCree = listArmoirePk[listArmoirePk.Count-1];
+                object lastArmoireCree = listArmoirePk[listArmoirePk.Count - 1];
                 Navigation.PushAsync(new Form(lastArmoireCree));
             }
             else
             {
-                // Les entrées ne sont pas valides, affichez un message d'erreur
-                DisplayAlert("Error", "Please enter valid dimensions.", "OK");
+                // Affichez un message d'erreur si la largeur est supérieure à la longueur
+                DisplayAlert("Error", "Width cannot be greater than length.", "OK");
             }
         }
+
     }
 }
