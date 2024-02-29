d2e2e62a28351193accc097bf82f08a1786c2c7d
GaripA <garip.akkaya9@gmail.com>
2024-02-29 15:15:02 +0100

    commit garip6

---
+++ KitBox/Views/ChoicebtwPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KitBox.Views;
+
+public partial class ChoicebtwPage : ContentPage
+{
+    public ChoicebtwPage()
+    {
+        InitializeComponent();
+    }
+
+    private async void OnredirectionOrdermaker(object sender, EventArgs e)
+    {
+
+        await (Navigation.PushAsync(new ShopKeeperPage()));
+        
+        
+    }
+    private async void OnredirectionCustomer(object sender, EventArgs e)
+    {
+
+        await (Navigation.PushAsync(new Customer_catalog()));
+        
+        
+    }
+    
+}
---
+++ KitBox/Views/ShopKeeperPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KitBox.Views;
+
+public partial class ShopKeeperPage : ContentPage
+{
+    public ShopKeeperPage()
+    {
+        InitializeComponent();
+    }
+}
