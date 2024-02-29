d618f62684a988d60f722e7a4f304a388cbdf84e
GaripA <garip.akkaya9@gmail.com>
2024-02-29 15:27:06 +0100

    commit garip10

--- KitBox/Views/Form.xaml.cs
+++ KitBox/Views/Form.xaml.cs
@@ -1,6 +1,11 @@
 using DAL;
 using Org.BouncyCastle.Utilities;
-
+using Microsoft.Maui.Controls;
+using DAL;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using DevTools;
 namespace KitBox.Views
 {
     public partial class Form : ContentPage
