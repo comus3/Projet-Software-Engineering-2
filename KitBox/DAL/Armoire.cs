65c2a011a676b0c7c8d920c60cbb38f3ddeab58b
guilluy <baptisteguilluy0728@gmail.com>
2024-03-01 11:18:54 +0100

    primary key update

--- KitBox/DAL/Armoire.cs
+++ KitBox/DAL/Armoire.cs
@@ -6,6 +6,6 @@ public class Armoire : Model
     public Armoire(Connection connection) : base(connection)
     {
         tableName = "armoire";
-        primaryKey = "index";
+        primaryKey = "id_armoire";
     }
 }
