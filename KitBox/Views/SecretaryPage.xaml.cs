7d6ca5738b74136f23d760074eadfc3b9fac032c
GaripA <garip.akkaya9@gmail.com>
2024-03-01 13:47:28 +0100

    commit garip 15

--- KitBox/Views/SecretaryPage.xaml.cs
+++ KitBox/Views/SecretaryPage.xaml.cs
@@ -9,18 +9,40 @@ using DevTools;
 
 namespace KitBox.Views;
 
+using Microsoft.Maui.Controls;
+using Microsoft.Maui.Controls.Xaml;
+using DAL;
+using System;
+using System.Data;
+using DevTools;
+
+
 public partial class SecretaryPage : ContentPage
 {
     
     private Connection con;
-    
+    internal class itemsattributes
+        {
+            
+            public string Nom { get; set; }
+            
+            public string Price { get; set; }
+            public itemsattributes(string nom, string price)
+            {
+                Nom = nom; 
+                Price = price;
+            }
+        }
     public SecretaryPage()
     {
         InitializeComponent();
         BindingContext = this;
         Connection.TestConnection();
         con = new Connection();
+        
     }
+
+    
 
     private async void Modifier_Clicked(object sender, EventArgs e)
     {
