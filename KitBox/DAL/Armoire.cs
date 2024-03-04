17c2a7114eca1b0e66901f1fcbb11636141052df
comus3 <come.plantin.carrenard@gmail.com>
2024-03-04 23:23:04 +0100

    removed instant piece linkng because it is impossible to do since casiers have to be created first

--- KitBox/DAL/Armoire.cs
+++ KitBox/DAL/Armoire.cs
@@ -11,28 +11,4 @@ public class Armoire : Model
         tableName = "armoire";
         primaryKey = "id_armoire";
     }
-    public override DataTable Insert()
-    {
-        DataTable result = base.Insert();
-        object armoirePk = result.Rows[0][0];
-
-        if (armoirePk != null)
-        {
-            this.Load(Convert.ToInt32(armoirePk));
-            if (LinkingServices.CreateAllArmoireLinks(this.connection, this))
-            {
-                return result;
-            }
-            else
-            {
-                Logger.WriteToFile("Error creating links for casier.");
-                return null;
-            }
-        }
-        else
-        {
-            Logger.WriteToFile("Error inserting casier.");
-            return null;
-        }
-    }
 }
