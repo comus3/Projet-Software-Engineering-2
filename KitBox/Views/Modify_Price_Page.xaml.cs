8bb401ca8bba29bdcd9bf737593e9c56787d23f8
GaripA <garip.akkaya9@gmail.com>
2024-03-09 17:58:59 +0100

    commit garip 23

--- KitBox/Views/Modify_Price_Page.xaml.cs
+++ KitBox/Views/Modify_Price_Page.xaml.cs
@@ -10,23 +10,34 @@ namespace KitBox.Views;
 
 public partial class Modify_Price_Page : ContentPage
 {
-    public Modify_Price_Page(string oldPriceSupplier1, string oldPriceSupplier2)
+    private Connection con; 
+    private object cle;
+    public Modify_Price_Page(string oldPriceSupplier1, string oldPriceSupplier2, string Code)
     {
         InitializeComponent();
 
         OldPriceSupplier1Entry.Text = oldPriceSupplier1;
         OldPriceSupplier2Entry.Text = oldPriceSupplier2;
+        Connection.TestConnection();
+        con = new Connection();
+        cle = Code;
     }
 
     private void ConfirmButton_Clicked(object sender, EventArgs e)
     {
-        // Handle the logic for saving new prices and navigating back to SecretaryPage here
-        // You can access the new prices from OldPriceSupplier1Entry.Text and OldPriceSupplier2Entry.Text
-        string newPriceSupplier1 = OldPriceSupplier1Entry.Text;
-        string newPriceSupplier2 = OldPriceSupplier2Entry.Text;
-        
-        Navigation.PushAsync(new SecretaryPage()); // Update your data accordingly and navigate back to SecretaryPage
+      
+            // Handle the logic for saving new prices and navigating back to SecretaryPage here
+            // You can access the new prices from OldPriceSupplier1Entry.Text and OldPriceSupplier2Entry.Text
+            string newPriceSupplier1 = OldPriceSupplier1Entry.Text;
+            string newPriceSupplier2 = OldPriceSupplier2Entry.Text;
 
+            Piece piece_spec = new Piece(con);
+            piece_spec.Load(cle);
+           
+            Logger.WriteToFile(piece_spec);
+            Navigation.PushAsync(new SecretaryPage()); // Update your data accordingly and navigate back to SecretaryPage
+        
+        
     }
 
 }
