4eeaa340b3d03a56ac1eff35c4ba64e5cf90e0a9
comus3 <come.plantin.carrenard@gmail.com>
2024-03-14 16:37:44 +0100

    price armoire ou casier implemented and working

--- KitBox/AppServices/PricingServices.cs
+++ KitBox/AppServices/PricingServices.cs
@@ -1,19 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DAL;
+using DevTools;
+using KitBox.AppServices;
 namespace AppServices;
 
 class PricingServices
 {
-    public static void PriceCasier()
+    public static void PriceObject(Model obj ,Connection connection)
     {
+        DataTable pieces = new DataTable();
+        double price = 0;
 
-    }
-    public static void PriceArmoire()
-    {
-
+        if (obj.GetType() == typeof(Armoire))
+        {
+            pieces = FetchingServices.FetchArmoirePieces(connection, obj.Attributes[obj.PrimaryKey]);
+        }
+        else if (obj.GetType() == typeof(Casier))
+        {
+            pieces = FetchingServices.FetchCasierPieces(connection, obj.Attributes[obj.PrimaryKey]);
+        }
+        else
+        {
+            Logger.WriteToFile($"Error: Object {obj} not appropriate for pricing");
+        }
+        foreach (DataRow row in pieces.Rows)
+        {
+            price += Convert.ToDouble(row.ItemArray[10]);
+        }
+        obj.Attributes["price"] = price;
+        obj.Save();
     }
     public static void PriceCommande()
     {
