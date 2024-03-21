4109170981ce86c65de598a492f8f7045e445771
Soraya-j <21255@ecam.be>
2024-03-21 14:05:52 +0100

    add choice

--- KitBox/Views/ChoicebtwPage.xaml.cs
+++ KitBox/Views/ChoicebtwPage.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MauiApp1;
 
 namespace KitBox.Views;
 
@@ -16,10 +17,14 @@ public partial class ChoicebtwPage : ContentPage
     private async void OnredirectionOrdermaker(object sender, EventArgs e)
     {
 
-        await (Navigation.PushAsync(new OrderMakerPage()));
-        
-        
+        await (Navigation.PushAsync(new OrderMakerPage())); 
     }
+    private async void OnredirectionShopKeeper(object sender, EventArgs e)
+    {
+
+        await (Navigation.PushAsync(new ShopKeeperPage())); 
+    }
+
     private async void OnredirectionCustomer(object sender, EventArgs e)
     {
 
