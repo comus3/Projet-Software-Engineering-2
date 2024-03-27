e393935fe6f530a6196ec25d99699155cfbb5ff1
GaripA <garip.akkaya9@gmail.com>
2024-03-27 14:29:51 +0100

    commit garip 53

---
+++ KitBox/Views/StockmanagerPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AppServices;
+using DAL;
+namespace KitBox.Views;
+
+public partial class StockmanagerPage : ContentPage
+{
+    private Connection connection; 
+    
+    public StockmanagerPage()
+    {
+       // InitializeComponent();
+        connection = new Connection();
+        
+
+    }
+    
+    
+    
+}
