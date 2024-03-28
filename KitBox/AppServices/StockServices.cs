65a054dd7920d3a9a7da7c91f99fd64cbe6abeae
comus3 <come.plantin.carrenard@gmail.com>
2024-03-28 16:32:48 +0100

    modif

--- KitBox/AppServices/StockServices.cs
+++ KitBox/AppServices/StockServices.cs
@@ -277,6 +277,14 @@ class StockServices
                 
         }
     }
+    public static void AwaitAddQuantity(object code,int quantity,Connection connection)
+    {
+        Piece piece = new Piece(connection);
+        int awaitQtt = Convert.ToInt32(piece.Load(code).Rows[0].ItemArray[9]);
+        int newQtt = quantity + awaitQtt;
+        piece.Attributes["await"] = newQtt;
+        piece.Save();
+    }
 }
   
 
