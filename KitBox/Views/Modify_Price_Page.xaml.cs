ab628d94966d9a081be464767b6584c25b01678e
GaripA <garip.akkaya9@gmail.com>
2024-05-15 21:08:19 +0200

    correction finale

--- KitBox/Views/Modify_Price_Page.xaml.cs
+++ KitBox/Views/Modify_Price_Page.xaml.cs
@@ -1,63 +1,64 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using DAL;
-using DevTools;
-using System.Data ; 
-namespace KitBox.Views;
+﻿    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using DAL;
+    using DevTools;
+    using System.Data ; 
+    namespace KitBox.Views;
 
-public partial class Modify_Price_Page : ContentPage
-{
-    private Connection con; 
-    private object cle;
-    public Modify_Price_Page(string oldselling_price, string code)
+    public partial class Modify_Price_Page : ContentPage
     {
-        InitializeComponent();
-
-        OLDselling_price.Text = oldselling_price;
-        
-        Connection.TestConnection();
-        con = new Connection();
-        cle = code;
-    }
-
-    private void ConfirmButton_Clicked(object sender, EventArgs e)
-    {
-        try
+        private Connection con; 
+        private object cle;
+         private Entry OLDselling_price; 
+        public Modify_Price_Page(string oldselling_price, string code)
         {
-            
-            string newselling_price = OLDselling_price.Text;
-        
+            InitializeComponent();
 
-            Piece piece_spec = new Piece(con);
-           
-            DataTable result = piece_spec.Load(cle);
-           
-               
-                Dictionary<string, object> valuesToUpdate = new Dictionary<string, object>();
-                valuesToUpdate["selling_price"] = newselling_price;
+            OLDselling_price.Text = oldselling_price;
+            
+            Connection.TestConnection();
+            con = new Connection();
+            cle = code;
+        }
+
+        private void ConfirmButton_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
                 
-                piece_spec.Update(valuesToUpdate);
+                string newselling_price = OLDselling_price.Text;
+            
 
-               
-                piece_spec.Save();
+                Piece piece_spec = new Piece(con);
+            
+                DataTable result = piece_spec.Load(cle);
+            
+                
+                    Dictionary<string, object> valuesToUpdate = new Dictionary<string, object>();
+                    valuesToUpdate["selling_price"] = newselling_price;
+                    
+                    piece_spec.Update(valuesToUpdate);
 
-                Navigation.PushAsync(new SecretaryPage()); 
+                
+                    piece_spec.Save();
+
+                    Navigation.PushAsync(new SecretaryPage()); 
+                
+                
+            }
+            catch (Exception ex)
+            {
+                
+                Logger.WriteToFile($"Error occurred while updating price: {ex.Message}");
             
-            
+            }
         }
-        catch (Exception ex)
-        {
-            
-            Logger.WriteToFile($"Error occurred while updating price: {ex.Message}");
-           
-        }
+
+
     }
 
 
-}
-
-
-    
+        
